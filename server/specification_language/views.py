59e92c4926706757b64117d19db16a55a96aa603
Huỳnh Thanh Phong <huynhthanhphong0931@gmail.com>
2021-12-13 21:19:14 +0700

    Đã có thể load css và js

--- server/specification_language/views.py
+++ server/specification_language/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 
 def index(request):
     template = loader.get_template('html/Final_project.html')
+    # template = loader.get_template('html/staticfile.html')
     context = {
         'latest_question_list': '',
     }
